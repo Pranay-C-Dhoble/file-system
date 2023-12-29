d016d4cd3a6e9315041fd9621729428ee9dd41c0
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-12-29 14:57:23 +0530

    Create ComparePrice.java

---
+++ src/main/java/file_handling/compare/ComparePrice.java
@@ -0,0 +1,20 @@
+package file_handling.compare;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ComparePrice {
+    public static double comparePrice(List data) {
+        BigDecimal maxPrice = new BigDecimal(0);
+        for(Object d : data){
+            String[] data2 = d.toString().split(",");
+            String data3 = data2[5];
+            BigDecimal currentPrice = new BigDecimal(data3);
+            if (currentPrice.compareTo(maxPrice) > 0) {
+                maxPrice = currentPrice;
+            }
+        }
+        System.out.println(maxPrice);
+        return maxPrice.doubleValue();
+    }
+}
