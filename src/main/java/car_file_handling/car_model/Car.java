2b38e2b7f13fb88e59708a0074bb74f21ec7154b
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2023-12-29 14:57:20 +0530

    Update Car.java

--- src/main/java/car_file_handling/car_model/Car.java
+++ src/main/java/car_file_handling/car_model/Car.java
@@ -31,7 +31,25 @@ public class Car implements Comparable{
 
     @Override
     public int compareTo(Object o) {
-        Car car = (Car) o;
+        Car car = (Car) o; //type casting
         return this.transmission.compareTo(car.transmission);
     }
+
+    //how many types of casting in java
+    //1. primitive casting
+    //2. object casting
+    //3. generic casting
+
+    //primitive casting
+    //int a = 10;
+    //double b = a; //implicit casting
+    //double b = (double) a; //explicit casting
+
+    //object casting
+    //Object a = new Object();
+    //String b = (String) a; //explicit casting
+
+    //generic casting
+    //List a = new ArrayList();
+    //List<String> b = (List<String>) a; //explicit casting
 }
