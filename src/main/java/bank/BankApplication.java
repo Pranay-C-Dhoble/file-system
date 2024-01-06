93aeb02693681e53523a23cf4a02effce12cf17f
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2024-01-07 01:24:14 +0530

    Update BankApplication.java

--- src/main/java/bank/BankApplication.java
+++ src/main/java/bank/BankApplication.java
@@ -1,20 +1,35 @@
 package bank;
 
+import bank.listfile.Path;
 import bank.matrics.MatricsCollector;
+import bank.matrics.TransByDOBYear;
+import bank.matrics.TransByLocation;
 import bank.matrics.TransactionByGenderMatrics;
 import bank.model.BankTransaction;
-import bank.pathgenerator.Path;
-import bank.reader.TransactionReader;
+import bank.writer.CSVTransWriter;
 
 import java.util.List;
 
 public class BankApplication {
     public static void main(String[] args) {
-        TransactionReader reader = new TransactionReader();
-        String fileName = Path.find("src/main/resources/Bank_Output");
-        List<BankTransaction> transactions = reader.read(fileName);
-        MatricsCollector metrics = new TransactionByGenderMatrics();
-        System.out.println(transactions.size());
-        metrics.collect(transactions);
+        Path path = new Path();
+        path.find("src/main/resources/banK_Output");
+        List<BankTransaction> data = path.getAllDataList();
+
+        CSVTransWriter csvTransWriter = new CSVTransWriter();
+        csvTransWriter.csvWriter(data, "src/main/resources/Bank_Clean_data/bank_clean_transactions.csv");
+        System.out.println("Total Transactions: " + data.size());
+
+//        MatricsCollector metrics = new TransactionByGenderMatrics();
+//        System.out.println("Total Transactions: " + data.size());
+//        metrics.collect(data);
+//
+//        System.out.println("--------------------------------------------------");
+//        TransByLocation transByLocation = new TransByLocation();
+//        transByLocation.collect(data);
+//
+//        System.out.println("--------------------------------------------------");
+//        TransByDOBYear transByDOBYear = new TransByDOBYear();
+//        transByDOBYear.collect(data);
     }
 }
